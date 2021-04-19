54fe16b0fb8ccaa99a35666d6cef99e97d8e7ea8
JinHee8989 <wlsgml8989@gmail.com>
2021-04-19 15:07:22 +0900

    반쪽짜리 커밋
    	modified:   src/main/java/com/example/git/TestClass/Test1.java

--- src/main/java/com/example/git/TestClass/Test1.java
+++ src/main/java/com/example/git/TestClass/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
     public void test(){
         System.out.println("커밋을 할테얌");
         System.out.println("Modified상태의 파일을 commit하기");
+        System.out.println("완벽한 커밋");
     }
 
 }
